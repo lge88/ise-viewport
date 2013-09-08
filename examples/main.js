6d4713ce9b61161b33a90951e27b425c14747fbf
Li Ge <lge@ucsd.edu>
2013-09-08 04:48:55 -0700

    Update at Sun Sep  8 04:48:55 PDT 2013

--- examples/main.js
+++ examples/main.js
@@ -1,6 +1,7 @@
 var ISEViewport = require( 'ise-viewport' );
-var arrgen = require( 'arr-gen' );
+var randomCubes = require( 'three-random-cubes' );
 var THREE = require( 'three' );
+// var arrgen = require( 'arr-gen' );
 
 var container = document.getElementById( 'main' );
 var viewport = ISEViewport( { container: container } );
@@ -11,28 +12,15 @@ var scene = viewport.scene;
 var camera = viewport.camera;
 var workplane = viewport.workplane;
 var clock = new THREE.Clock();
-var cubes = [];
+var cubes = randomCubes( 100 );
+
+cubes.forEach( function( c ) { scene.add( c ); } );
 
 clock.start();
 renderer.setClearColor( 0xdddddd );
 window.onresize = function() {
   viewport.handleResize();
 };
-
-arrgen( 100, function( i ) {
-  return {
-    x: random( -100, 100 ),
-    y: random( -100, 100 ),
-    z: random( -100, 100 )
-  };
-} ).map( function( pos ) {
-  var c = cube();
-  c.position.copy( pos );
-  return c;
-} ).forEach( function( c ) {
-  cubes.push( c );
-  scene.add( c );
-} );
 
 var vel = new THREE.Vector3();
 
@@ -52,24 +40,12 @@ viewport.preStack.push( {
         random( -10, 10 )
       );
       c.position.add( vel );
+      c.material.color.set( randomColor() );
     } );
   }
 } );
 
-function cube( w, h, t ) {
-  var shininess = 50, specular = 0x333333, bumpScale = 1, shading = THREE.SmoothShading;
-  var m = new THREE.Mesh(
-    new THREE.CubeGeometry( w || 40, h || 40, t || 40 ),
-    new THREE.MeshPhongMaterial( { color: randomColor(), ambient: 0x000000, specular: 0x00ffaa, shininess: shininess, metal: true, shading: shading } )
-  );
-  m.material.wireframe = false;
-  return m;
-}
-
+function random( a, b ) { return Math.random()*( b - a ) + a; }
 function randomColor() {
-  return '#'+Math.floor(Math.random()*16777215).toString(16);
-}
-
-function random( a, b ) {
-  return Math.random()*( b - a ) + a;
+  return '#' + Math.floor( Math.random()*16777215 ).toString( 16 );
 }
